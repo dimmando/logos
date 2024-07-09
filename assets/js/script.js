7f61cebe290850ca9158c4d83b335e3e05575cb3
Dymtro Chesnokov <dimmando@ukr.net>
2024-07-09 19:21:15 +0000

    Add function

--- assets/js/script.js
+++ assets/js/script.js
@@ -178,6 +178,7 @@ document.addEventListener("DOMContentLoaded", () => {
         incorrectScore.textContent = incorrectAnswers;
     }
     function totalQuestionsLeft() {
-        totalLeft = currentQuestions.length - currentQuestionIndex;
+        totalLeft = logoEasyQuestions.length - currentQuestionIndex;
+        console.log(totalLeft);
     }    
 });
